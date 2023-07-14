d6c048f1daa6aab3f1a5093f0999cb80cfd4d194
dima <cinnamonbun1233@yandex.ru>
2023-07-14 09:47:05 +0300

    fix: переписал stats server на JPQL

--- stats-service/stats-service-server/src/main/java/ewm_server/repo/StatsRepo.java
+++ stats-service/stats-service-server/src/main/java/ewm_server/repo/StatsRepo.java
@@ -1,7 +1,10 @@
 package ewm_server.repo;
 
+import ewm_dto.domain_dto.StatsResponseDto;
 import ewm_server.model.StatsRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -10,7 +13,39 @@ import java.util.List;
 @Repository
 public interface StatsRepo extends JpaRepository<StatsRecord, Long> {
 
-    List<StatsRecord> findAllByTimestampBetween(LocalDateTime start, LocalDateTime end);
+    @Query("SELECT new ewm_dto.domain_dto.StatsResponseDto(r.app, r.uri, COUNT(r.uri)) " +
+            "FROM StatsRecord as r " +
+            "WHERE r.timestamp BETWEEN :start AND :end " +
+            "GROUP BY r.app, r.uri " +
+            "ORDER BY COUNT(r.uri) DESC")
+    List<StatsResponseDto> getStatsForDates(@Param("start") LocalDateTime start,
+                                            @Param("end") LocalDateTime end);
 
-    List<StatsRecord> findAllByTimestampBetweenAndUriIn(LocalDateTime start, LocalDateTime end, String[] uris);
+    @Query("SELECT new ewm_dto.domain_dto.StatsResponseDto(r.app, r.uri, COUNT(r.uri)) " +
+            "FROM StatsRecord as r " +
+            "WHERE r.timestamp BETWEEN :start AND :end " +
+            "AND r.uri IN :uris " +
+            "GROUP BY r.app, r.uri " +
+            "ORDER BY COUNT(r.uri) DESC")
+    List<StatsResponseDto> getStatsForDatesAndUris(@Param("start") LocalDateTime start,
+                                                   @Param("end") LocalDateTime end,
+                                                   @Param("uris") String[] uris);
+
+    @Query("SELECT new ewm_dto.domain_dto.StatsResponseDto(r.app, r.uri, COUNT(DISTINCT r.ip)) " +
+            "FROM StatsRecord as r " +
+            "WHERE r.timestamp BETWEEN :start AND :end " +
+            "GROUP BY r.app, r.uri " +
+            "ORDER BY COUNT(DISTINCT r.ip) DESC")
+    List<StatsResponseDto> getStatsForDatesWithUniqueIp(@Param("start") LocalDateTime start,
+                                                        @Param("end") LocalDateTime end);
+
+    @Query("SELECT new ewm_dto.domain_dto.StatsResponseDto(r.app, r.uri, COUNT(DISTINCT r.ip)) " +
+            "FROM StatsRecord as r " +
+            "WHERE r.timestamp BETWEEN :start AND :end " +
+            "AND r.uri IN :uris " +
+            "GROUP BY r.app, r.uri " +
+            "ORDER BY COUNT(DISTINCT r.ip) DESC")
+    List<StatsResponseDto> getStatsForDatesAndUrisWithUniqueIp(@Param("start") LocalDateTime start,
+                                                               @Param("end") LocalDateTime end,
+                                                               @Param("uris") String[] uris);
 }
--- stats-service/stats-service-server/src/main/java/ewm_server/service/StatsServiceImpl.java
+++ stats-service/stats-service-server/src/main/java/ewm_server/service/StatsServiceImpl.java
@@ -3,17 +3,13 @@ package ewm_server.service;
 import ewm_dto.domain_dto.StatsRequestDto;
 import ewm_dto.domain_dto.StatsResponseDto;
 import ewm_server.mapper.StatsMapper;
-import ewm_server.model.StatsRecord;
 import ewm_server.repo.StatsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class StatsServiceImpl implements StatsService {
@@ -32,30 +28,22 @@ public class StatsServiceImpl implements StatsService {
 
     @Override
     public List<StatsResponseDto> getStats(String start, String end, String[] uris, String unique) {
-        List<StatsRecord> recordsFound;
-        if (uris == null) {
-            recordsFound = statsRepo.findAllByTimestampBetween(parseDateTime(start), parseDateTime(end));
+        if (unique == null && uris == null) {
+            return statsRepo.getStatsForDates(parseDateTime(start), parseDateTime(end));
+        } else if (unique != null && uris == null) {
+            if (Boolean.parseBoolean(unique)) {
+                return statsRepo.getStatsForDatesWithUniqueIp(parseDateTime(start), parseDateTime(end));
+            } else {
+                return statsRepo.getStatsForDates(parseDateTime(start), parseDateTime(end));
+            }
+        } else if (unique == null && uris != null) {
+            return statsRepo.getStatsForDatesAndUris(parseDateTime(start), parseDateTime(end), uris);
         } else {
-            recordsFound = statsRepo.findAllByTimestampBetweenAndUriIn(parseDateTime(start),
-                    parseDateTime(end), uris);
-        }
-        if (unique == null) {
-            Map<String, Long> hitsForUriMap = recordsFound.stream()
-                    .collect(Collectors.groupingBy(StatsRecord::getUri, Collectors.counting()));
-            return recordsFound.stream().map(r -> new StatsResponseDto(r.getApp(), r.getUri(),
-                            hitsForUriMap.get(r.getUri())))
-                    .distinct()
-                    .sorted(Comparator.comparing(StatsResponseDto::getHits).reversed())
-                    .collect(Collectors.toList());
-        } else {
-            Map<String, Map<String, Long>> hitsForUriAndIpMap = recordsFound.stream()
-                    .collect(Collectors.groupingBy(StatsRecord::getUri,
-                            Collectors.groupingBy(StatsRecord::getIp, Collectors.counting())));
-            return recordsFound.stream().map(r -> new StatsResponseDto(r.getApp(), r.getUri(),
-                            Long.valueOf(hitsForUriAndIpMap.get(r.getUri()).size())))
-                    .distinct()
-                    .sorted(Comparator.comparing(StatsResponseDto::getHits).reversed())
-                    .collect(Collectors.toList());
+            if (Boolean.parseBoolean(unique)) {
+                return statsRepo.getStatsForDatesAndUrisWithUniqueIp(parseDateTime(start), parseDateTime(end), uris);
+            } else {
+                return statsRepo.getStatsForDatesAndUris(parseDateTime(start), parseDateTime(end), uris);
+            }
         }
     }
 
